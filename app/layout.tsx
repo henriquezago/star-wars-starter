e1a06adfd8c50ae24aafa16c982f9f8589158d17
Henrique Zago <lobato.zago@gmail.com>
2023-05-13 21:36:06 +0100

    feat: Montserrat font and layout header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Montserrat } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Star Wars Archives",
@@ -15,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="header">SWStarter</header>
+        {children}
+      </body>
     </html>
   );
 }
